b7a3cf6c087c1e86ca2bac4374f0c2f16da1f962
Tian Biao <btian@thoughtworks.com>
2017-01-15 19:25:19 +1100

    UI change for home page

--- config/desktop_navigation.rb
+++ config/desktop_navigation.rb
@@ -1,5 +1,8 @@
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.active_leaf_class = 'navigation-item'
+
   navigation.items do |primary|
+    primary.dom_class = 'navigation'
     primary.item :desktop_navigation_home, 'Home', '/'
     primary.item :desktop_navigation_my_posts, 'My posts', '/'
     primary.item :desktop_navigation_about_me, 'About me', '/'
